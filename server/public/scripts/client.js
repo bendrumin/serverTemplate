000413162df432731ca53f95841582263e7a7692
Ben Siegel <bendrumin@Bens-MacBook-Pro-2.local>
2017-08-01 20:56:47 -0500

    OOP Homework and adding a server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,7 @@
 console.log('JS Sourced');
+var myApp = angular.module('myApp', []);  //Will leave empty for now
 
-  class CoffeeMaker {
-    constructor(cups, brew) {
-      this.cups = cups; // Maximum capacity of the container
-      this.brew = brew; // Color of the water maker
-      this.maxCups = 0; // Amount of liquid the container holds
-    }
+myApp.controller('WelcomeController', function(){
 
-    // Add liquid if space is available
-    fill(waterResevour) {
-      if(this.maxCups + waterResevour <= this.cups) {
-        this.maxCups = this.maxCups + waterResevour;
-      } else {
-        console.log('Coffee Maker is full!');
-      }
-    }
-  }
 
-  let maker = new CoffeeMaker(10, 'dark');
-
-  console.log(maker.maxCups);
-  maker.fill(2);
-  console.log(maker.maxCups); // Will log 8
-  maker.fill(5); // A total of 12 would put us over the limit
-  console.log(maker.maxCups); // Will log 8
+});
